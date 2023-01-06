da7d7f47e51eddc825656598cc64b16ad0a5e565
battlmonstr <battlmonstr@users.noreply.github.com>
2023-01-06 11:37:36 +0100

    update Package.swift to WireGuardKitGo 1.0.15.11 release

--- Package.swift
+++ Package.swift
@@ -25,7 +25,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "WireGuardKitGo",
-            path: "Sources/WireGuardKitGo/out/WireGuardKitGo.xcframework"
+            url: "https://github.com/battlmonstr/wireguard-apple/releases/download/1.0.15.11/WireGuardKitGo.xcframework.zip",
+            checksum: "54f50b13f145007f94e420205830d3df5cddef4a32ce3eaa0106e4e17906ac29"
         )
     ]
 )
