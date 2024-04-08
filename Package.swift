75d89a0b66a77c9978b39d15d8eb408ae7d25413
Daniel Bratell <bratell@opera.com>
2024-04-08 11:48:22 +0200

    Updated links to binary release of 1.0.15.15

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "WireGuardKitGo",
-            url: "https://github.com/battlmonstr/wireguard-apple/releases/download/1.0.15.13/WireGuardKitGo.xcframework.zip",
-            checksum: "c01d7c8d2523f23f1287871af482c3c464e0a9a6062445e036995fbdedb93498"
+            url: "https://github.com/operasoftware/wireguard-apple/releases/download/1.0.15.15/WireGuardKitGo.xcframework.zip",
+            checksum: "2b25a9218c693c2d131d0c00303d15340ffe8ad68e20e0ef7a20b287781050c2"
         )
     ]
 )
